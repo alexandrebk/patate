86449f022677dd39d45557a6aac5cff0063a964b
Alexandre Bouvier <32016201+alexandrebk@users.noreply.github.com>
2022-11-14 12:22:55 +0100

    refactor: transfer logic from controller in model

--- app/controllers/api/potato_quotations_controller.rb
+++ app/controllers/api/potato_quotations_controller.rb
@@ -5,22 +5,7 @@ module Api
     end
 
     def maximum_gain
-      # TODO : move logic in model
-      potato_quotations = PotatoQuotation.today
-
-      previous_price = nil
-      gains = potato_quotations.map do |potato|
-        if previous_price.nil?
-          previous_price = potato.price_per_ton
-          0
-        else
-          diff = [(potato.price_per_ton - previous_price), 0].max
-          previous_price = potato.price_per_ton
-          diff * PotatoQuotation::MAX_PURCHASE_PER_DAY
-        end
-      end
-
-      @maximum_gain = gains.sum
+      @maximum_gain = PotatoQuotation.max_possible_gains_for_today.sum
     end
   end
 end
--- app/models/potato_quotation.rb
+++ app/models/potato_quotation.rb
@@ -1,9 +1,26 @@
 class PotatoQuotation < ApplicationRecord
-  MAX_PURCHASE_PER_DAY=100
+  QUANTITY_MAX_PER_DAY=100
 
   default_scope { order(:priced_at) }
 
   validates :price_per_ton, :priced_at, presence: true
 
   scope :today, -> { where("priced_at >= ? OR priced_at < ?", Date.today, Date.today + 1) }
+
+  def possible_gain(price)
+    return 0 if price.nil?
+    return 0 if price >= price_per_ton
+
+    price_per_ton - price
+  end
+
+  class << self
+    def max_possible_gains_for_today
+      PotatoQuotation.today.map do |potato|
+        possible_gain = potato.possible_gain(@previous_price)
+        @previous_price = potato.price_per_ton
+        possible_gain * QUANTITY_MAX_PER_DAY
+      end
+    end
+  end
 end
